42c3a01d3da6be47ca403697d4c02c93cbd02f5b
kirtikumar <kirtikumar.avaiya@provabmail.com>
2023-11-30 16:01:48 +0530

    FIX MOBILE VIEW

--- lib/feature/tutorial/mobileEnter/view/mobile_view.dart
+++ lib/feature/tutorial/mobileEnter/view/mobile_view.dart
@@ -4,7 +4,6 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 import '../../../../custom/app_textformfield/text_field.dart';
-import '../../../../custom/appbar/appbar.dart';
 import '../../../../custom/image/app_image_assets.dart';
 import '../../../../custom/preLoginWidget/pre_login_widget.dart';
 import '../../../../custom/text/app_text.dart';
@@ -15,8 +14,7 @@ import '../../view/bottomSheets/country_code_bottom_sheet.dart';
 import '../viewModel/mobile_view_model.dart';
 
 class MobileView extends StatelessWidget {
-  final bool? simpleAppBar;
-  const MobileView({super.key, this.simpleAppBar});
+  const MobileView({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +22,8 @@ class MobileView extends StatelessWidget {
         viewModel: MobileViewModel(),
         onModelReady: (MobileViewModel mobileViewModel) {
           mobileViewModel.setContext(context);
-          mobileViewModel.data = ModalRoute.of(context)!.settings.arguments! as Map;
+          mobileViewModel.data =
+              ModalRoute.of(context)!.settings.arguments! as Map;
           logs('argue--> ${mobileViewModel.data}');
           mobileViewModel.init();
         },
@@ -33,200 +32,107 @@ class MobileView extends StatelessWidget {
               //warnWhenNoObservables: false,
               builder: (BuildContext context) {
             return Scaffold(
-              body: simpleAppBar == false
-                  ? PreLoginCustomBody(
-                      widget: Expanded(
-                        child: ListView(
-                          padding: EdgeInsets.symmetric(horizontal: 15.px),
-                          children: <Widget>[
-                            SizedBox(height: 35.px),
-                            const OnTapBack(),
-                            SizedBox(height: 80.px),
-                            Align(
-                              alignment: Alignment.centerLeft,
-                              child: AppImageAsset(
-                                image: ImageConstants.primaryLogoBlue,
-                                height: 45.px,
-                              ),
-                            ),
-                            SizedBox(height: 28.px),
-                            AppText(
-                              textAlign: TextAlign.start,
-                              'join'.tr(),
-                              fontSize: 24.px,
-                              fontWeight: FontWeight.w700,
-                            ),
-                            SizedBox(height: 10.px),
-                            TextFormsField(
-                              prefix: (mobileViewModel.countries.isNotEmpty)
-                                  ? GestureDetector(
-                                      onTap: () {
-                                        showModalBottomSheet(
-                                          isScrollControlled: true,
-                                          context: context,
-                                          shape: const RoundedRectangleBorder(
-                                            borderRadius: BorderRadius.vertical(
-                                              top: Radius.circular(25.0),
-                                            ),
-                                          ),
-                                          builder: (BuildContext context) {
-                                            return StatefulBuilder(
-                                              builder: (BuildContext context,
-                                                  setState) {
-                                                return CountryCodeBottomsSheet(
-                                                    setState: setState,
-                                                    mobileViewModel:
-                                                        mobileViewModel);
-                                              },
-                                            );
-                                          },
-                                        );
-                                      },
-                                      child: Row(
-                                        children: [
-                                          SizedBox(width: 6.px),
-                                          ClipRRect(
-                                            borderRadius:
-                                                BorderRadius.circular(700),
-                                            child: AppImageAsset(
-                                              image: mobileViewModel
-                                                  .countries[mobileViewModel
-                                                      .countryIndex]
-                                                  .flag_url!,
-                                              // mobileViewModel!
-                                              //         .filteredCountries.isNotEmpty!
-                                              //     ? mobileViewModel!
-                                              //         .filteredCountries[
-                                              //             mobileViewModel
-                                              //                 .countryIndex]
-                                              //         .flag_url!
-                                              //     : mobileViewModel!
-                                              //         .countries[mobileViewModel
-                                              //             .countryIndex]
-                                              //         .flag_url!,
-                                              height: 16.px,
-                                              width: 16.px,
-                                            ),
-                                          ),
-                                          SizedBox(width: 6.px),
-                                          AppText(
-                                            mobileViewModel
-                                                .countries[mobileViewModel
-                                                    .countryIndex]
-                                                .idd_code
-                                                .toString(),
-                                            fontWeight: FontWeight.w400,
-                                            fontSize: 14.px,
-                                          )
-                                        ],
+              body: PreLoginCustomBody(
+                widget: Expanded(
+                  child: ListView(
+                    padding: EdgeInsets.symmetric(horizontal: 15.px),
+                    children: [
+                      SizedBox(height: 35.px),
+                      const OnTapBack(),
+                      SizedBox(height: 80.px),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: AppImageAsset(
+                            image: ImageConstants.primaryLogoBlue,
+                            height: 45.px),
+                      ),
+                      SizedBox(height: 28.px),
+                      AppText(
+                        textAlign: TextAlign.start,
+                        'join'.tr(),
+                        fontSize: 24.px,
+                        fontWeight: FontWeight.w700,
+                      ),
+                      SizedBox(height: 10.px),
+                      TextFormsField(
+                        prefix: (mobileViewModel.countries.isNotEmpty ||
+                                mobileViewModel.filteredCountries.isNotEmpty)
+                            ? GestureDetector(
+                                onTap: () {
+                                  showModalBottomSheet(
+                                    isScrollControlled: true,
+                                    context: context,
+                                    shape: const RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.vertical(
+                                        top: Radius.circular(25.0),
                                       ),
-                                    )
-                                  : SizedBox(),
-                              title: 'mobileNumber'.tr(),
-                              keyboardType: TextInputType.number,
-                              controller: mobileViewModel.mobileController,
-                              hintText: 'enterMobileAgain'.tr(),
-                              validate: mobileViewModel.mobileValid,
-                              errorText: mobileViewModel.mobileErrorText,
-                              onChanged: (String value) {
-                                mobileViewModel.validateMobile(value);
-                              },
-                            ),
-                            SizedBox(height: 20.px),
-                          ],
-                        ),
-                      ),
-                    )
-                  : Scaffold(
-                      appBar: HessaAppBar(
-                        title: 'Add Mobile Number',
-                        isTitleOnly: true,
-                      ),
-                      body: Expanded(
-                        child: ListView(
-                          padding: EdgeInsets.symmetric(horizontal: 15.px),
-                          children: <Widget>[
-                            SizedBox(height: 45.px),
-                            TextFormsField(
-                              prefix: (mobileViewModel.countries.isNotEmpty)
-                                  ? GestureDetector(
-                                      onTap: () {
-                                        showModalBottomSheet(
-                                          isScrollControlled: true,
-                                          context: context,
-                                          shape: const RoundedRectangleBorder(
-                                            borderRadius: BorderRadius.vertical(
-                                              top: Radius.circular(25.0),
-                                            ),
-                                          ),
-                                          builder: (BuildContext context) {
-                                            return StatefulBuilder(
-                                              builder: (BuildContext context,
-                                                  setState) {
-                                                return CountryCodeBottomsSheet(
-                                                    setState: setState,
-                                                    mobileViewModel:
-                                                        mobileViewModel);
-                                              },
-                                            );
-                                          },
-                                        );
-                                      },
-                                      child: Row(
-                                        children: [
-                                          SizedBox(width: 6.px),
-                                          ClipRRect(
-                                            borderRadius:
-                                                BorderRadius.circular(700),
-                                            child: AppImageAsset(
-                                              image: mobileViewModel
-                                                  .countries[mobileViewModel
-                                                      .countryIndex]
-                                                  .flag_url!,
-                                              // mobileViewModel!
-                                              //         .filteredCountries.isNotEmpty!
-                                              //     ? mobileViewModel!
-                                              //         .filteredCountries[
-                                              //             mobileViewModel
-                                              //                 .countryIndex]
-                                              //         .flag_url!
-                                              //     : mobileViewModel!
-                                              //         .countries[mobileViewModel
-                                              //             .countryIndex]
-                                              //         .flag_url!,
-                                              height: 16.px,
-                                              width: 16.px,
-                                            ),
-                                          ),
-                                          SizedBox(width: 6.px),
-                                          AppText(
-                                            mobileViewModel
-                                                .countries[mobileViewModel
-                                                    .countryIndex]
-                                                .idd_code
-                                                .toString(),
-                                            fontWeight: FontWeight.w400,
-                                            fontSize: 14.px,
-                                          )
-                                        ],
+                                    ),
+                                    builder: (BuildContext context) {
+                                      return StatefulBuilder(
+                                        builder:
+                                            (BuildContext context, setState) {
+                                          return CountryCodeBottomsSheet(
+                                              setState: setState,
+                                              mobileViewModel: mobileViewModel);
+                                        },
+                                      );
+                                    },
+                                  );
+                                },
+                                child: Row(
+                                  children: [
+                                    SizedBox(width: 6.px),
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(700),
+                                      child: AppImageAsset(
+                                        image: mobileViewModel
+                                                .selectedCountry?.flag_url ??
+                                            ImageConstants.globe,
+                                        fit: BoxFit.fill,
+                                        // ? mobileViewModel.filteredCountries[mobileViewModel.countryIndex].flag_url!
+                                        // : mobileViewModel.countries[mobileViewModel.countryIndex].flag_url!,
+                                        // mobileViewModel!
+                                        //         .filteredCountries.isNotEmpty!
+                                        //     ? mobileViewModel!
+                                        //         .filteredCountries[
+                                        //             mobileViewModel
+                                        //                 .countryIndex]
+                                        //         .flag_url!
+                                        //     : mobileViewModel!
+                                        //         .countries[mobileViewModel
+                                        //             .countryIndex]
+                                        //         .flag_url!,
+                                        height: 16.px,
+                                        width: 16.px,
                                       ),
+                                    ),
+                                    SizedBox(width: 6.px),
+                                    AppText(
+                                      mobileViewModel.selectedCountry?.idd_code
+                                              ?.toString() ??
+                                          'select',
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 14.px,
                                     )
-                                  : SizedBox(),
-                              title: 'mobileNumber'.tr(),
-                              keyboardType: TextInputType.number,
-                              controller: mobileViewModel.mobileController,
-                              hintText: 'enterMobileAgain'.tr(),
-                              validate: mobileViewModel.mobileValid,
-                              errorText: mobileViewModel.mobileErrorText,
-                              onChanged: (String value) {
-                                mobileViewModel.validateMobile(value);
-                              },
-                            ),
-                            SizedBox(height: 20.px),
-                          ],
-                        ),
+                                  ],
+                                ),
+                              )
+                            : const SizedBox(),
+                        title: 'mobileNumber'.tr(),
+                        keyboardType: TextInputType.number,
+                        controller: mobileViewModel.mobileController,
+                        hintText: 'enterMobileAgain'.tr(),
+                        validate: mobileViewModel.mobileValid,
+                        errorText: mobileViewModel.mobileErrorText,
+                        onChanged: (String value) {
+                          mobileViewModel.validateMobile(value);
+                        },
                       ),
-                    ),
+                      SizedBox(height: 20.px),
+                    ],
+                  ),
+                ),
+              ),
               bottomNavigationBar: PreLoginCommonButton(
                 title: 'continue'.tr(),
                 onTap: () => mobileViewModel.onTapMobileSubmit(),
