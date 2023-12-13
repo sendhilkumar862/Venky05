834c82005cf01cecf22e4d41948f54461e6257d3
arispvtltd <mayank.shekher@rootinfosol.com>
2023-12-13 20:08:42 +0530

    dashboard tap restriction api integration.

--- lib/feature/home_views/viewsModel/home_views_model.dart
+++ lib/feature/home_views/viewsModel/home_views_model.dart
@@ -1,8 +1,14 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:mobx/mobx.dart';
 
 import '../../../../product/base/model/base_view_model.dart';
+import '../../../custom/loader/easy_loader.dart';
+import '../../../product/base/model/base_model.dart';
+import '../../../product/utils/validators.dart';
+import '../model/home_views_dashboard_details.dart';
 import '../views/tabs/avtivities_view.dart';
 import '../views/tabs/classes_view.dart';
 import '../views/tabs/teachers_view.dart';
@@ -16,7 +22,9 @@ abstract class _HomeViewsModelBase extends BaseViewModel with Store {
   void setContext(BuildContext context) => viewModelContext = context;
 
   @override
-  void init() {}
+  void init() {
+   fetchDashBoardDetails();
+  }
 
   @observable
   int selectedIndex = 0;
@@ -34,6 +42,31 @@ abstract class _HomeViewsModelBase extends BaseViewModel with Store {
     selectedIndex = index;
     if (kDebugMode) {
       print(index);
+    }
+  }
+
+  Future<void> fetchDashBoardDetails() async {
+    showLoading();
+    logs('Getting Dashboard Details');
+    final Dio dio = Dio();
+    try {
+      Response response = await dio.get('http://167.99.93.83/api/v1/users/dashboard/details');
+
+      logs('Status code--> ${response.statusCode}');
+
+      if (response.statusCode == 200) {
+        EasyLoading.dismiss();
+        final BaseResponse<DashBoardDetailsModel> baseResponse =
+        BaseResponse<DashBoardDetailsModel>.fromJson(response.data as Map<String, dynamic>, DashBoardDetailsModel.fromJson);
+        var value=baseResponse.data.item!;
+
+        logs('Status code--> ${baseResponse.status.type}');
+        // countries = baseResponse.data.items ?? <Country>[];
+        // temp = baseResponse.data.items ?? <Country>[];
+      }
+    } catch (error) {
+      EasyLoading.dismiss();
+      logs('Error: $error');
     }
   }
 }
