a535a156ad21f25841149698fb3ea3cf58fd519a
arispvtltd <mayank.shekher@rootinfosol.com>
2023-12-28 15:33:13 +0530

    Add a message controller

---
+++ lib/feature/tutorial/messages/controller/message_controller.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart' hide Response;
+
+import '../../../../product/network/local/key_value_storage_service.dart';
+
+
+class MessageController extends GetxController{
+  final KeyValueStorageService keyValueStorageService =
+  KeyValueStorageService();
+  TextEditingController searchTeacherController = TextEditingController();
+  RxInt selectedIndex = 1.obs;
+  RxList<bool> isSelectReadStatus = [false, false].obs;
+  RxList<bool> isSelectNewStatus = [false, false].obs;
+
+
+  RxList readStatus = <String>['Read', 'Unread'].obs;
+  RxList newStatus = <String>['New to Old', 'Old to View'].obs;
+  RxBool teacherLongPress = false.obs;
+  RxBool isSelect = false.obs;
+  RxBool selectAll = false.obs;
+  int? formattedDate;
+  RxList selectedItems = [].obs;
+  RxList isSelected = List.filled(50, false).obs;
+
+
+  void onSelected(int index) {
+    isSelected[index] = !isSelected[index];
+  }
+
+
+  void onSelectAll(Function setState) {
+    selectAll.value = !selectAll.value;
+    if (selectAll.value) {
+      isSelected.value = List.filled(50, true);
+    } else {
+      isSelected.value = List.filled(50, false);
+    }
+  }
+
+
+
+}
