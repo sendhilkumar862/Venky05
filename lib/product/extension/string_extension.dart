eae1a5f2a283a063002a6138ed6536cf64f388ad
Ikyam2023 <128222453+Ikyam2023@users.noreply.github.com>
2023-12-29 00:39:49 +0530

    updated the logic...

--- lib/product/extension/string_extension.dart
+++ lib/product/extension/string_extension.dart
@@ -15,13 +15,17 @@ extension StringLocalization on String {
   }
 
   //get the name of the User
- String extractInitials() {
-  final List<String> names = split(' ');
+String extractInitials(String input) {
+  final List<String> names = input.split(' ');
   String initials = '';
 
   for (final String name in names) {
     if (name.isNotEmpty) {
-      initials += name.substring(0, 2).toUpperCase();
+      initials += name.substring(0, 1).toUpperCase();
+      if (initials.length < 2) {
+        // If the length is still less than 2, consider the next character
+        initials += name.length > 1 ? name.substring(1, 2).toUpperCase() : '';
+      }
       if (initials.length >= 2) {
         break; // Limit the result to a total of two characters
       }
