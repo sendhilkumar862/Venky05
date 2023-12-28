f0da53bb384381ba72c3819b0713c0b032796764
arispvtltd <mayank.shekher@rootinfosol.com>
2023-12-28 16:39:55 +0530

    Add a rating controller

--- lib/feature/tutorial/chat/controller/chat_controller.dart
+++ lib/feature/tutorial/chat/controller/chat_controller.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/cupertino.dart';
-import 'package:get/get.dart' hide Response;
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:intl/intl.dart';
 import '../../../../product/network/local/key_value_storage_service.dart';
 
