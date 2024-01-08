0d39e10663dce043eb07aea5a98b76d1e4fca607
ratnesh <ratneshkumar.singh@rootinfosolutions.net>
2024-01-08 12:38:55 +0530

    Removed unused code and files

--- lib/product/constants/typedefs.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-typedef JSON = Map<String, dynamic>;
-typedef QueryParams = Map<String, String>;
-typedef RouteBuilder = Widget Function(BuildContext);
-typedef ItemBuilder<T> = Widget Function(BuildContext, T);
